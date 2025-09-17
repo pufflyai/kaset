b444a350512b0be4c32c02d5e877158928c1c611
Aurélien Franky <au.re@live.com>
2025-09-17 17:52:24 +0200

    setup plugin package

---
+++ packages/@pstdio/kaset-plugin-host/vite.config.ts
@@ -0,0 +1,23 @@
+import { externalizeDeps } from "@au-re/vite-plugin-externalize-deps";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "kaset-plugin-host",
+      fileName: () => `index.js`,
+      formats: ["es"],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [
+    dts(),
+    externalizeDeps({
+      include: [/react$/],
+    }),
+  ],
+});
