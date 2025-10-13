1d37562ab4aef418a9599dfca9a8835dc17de573
Aurélien Franky <au.re@live.com>
2025-10-13 16:55:14 +0200

    fix

--- packages/@pstdio/tiny-ui/src/react/tiny-ui.tsx
+++ packages/@pstdio/tiny-ui/src/react/tiny-ui.tsx
@@ -127,6 +127,7 @@ export const TinyUI = forwardRef<TinyUIHandle, TinyUIProps>(function TinyUI(prop
         ref={iframeRef}
         title={title}
         src={runtimePath}
+        allowTransparency
         style={{ flex: 1, width: "100%", height: "100%", border: 0 }}
       />
     </div>
